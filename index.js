c2d46dac558566ad5c5acc7d107ccbd0490efb87
TheJulienM <julien.marfella77@gmail.com>
2021-04-04 18:33:38 +0200

    Processing of the number sent.
    Returns number in an array in JSON format. Displaying the number received in a span element. No convertion system yet

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ server.get('/', function(request, response) {
 
 server.get('/convertNumber', function(request, response) {
     var numberToConvert = request.query.arabicNumber;
-    console.log("numberToConvert " + numberToConvert)
+    var numberConverted;
+    var resultJson = [];
+    if(numberToConvert < 0 || numberToConvert > 100 || numberToConvert % 1 != 0)
+    {
+        numberConverted = "Votre nombre doit être compris entre 0 et 100 et doit être un nombre entier (sans virgule)";
+    }
+
+    else if (numberToConvert == 0)
+    {
+        numberConverted = "Les Romains n'ont pas de 0."
+    }
+
+    else
+    {
+        numberConverted = numberToConvert;
+    }
+    resultJson.push({"numberToConvert" : numberToConvert,"numberConverted":numberConverted})
+    response.contentType('application/json');
+    response.send(JSON.stringify(resultJson))
 });
 
